7c6ee8f2b4b6b04294411a50d81071ac107ccbf4
Hexenon <enriquebenavidesm@gmail.com>
2013-07-30 09:32:29 -0600

    Update foxpro_run.py

--- foxpro_run.py
+++ foxpro_run.py
@@ -5,12 +5,11 @@ import re
 class foxpro_run(sublime_plugin.WindowCommand):  
     def run(self):
         self.window.show_input_panel("Do ", "", self.on_done, None, None)
-        pass
+    	pass
 
     def on_done(self,user_input):
-      if user_input == "":
+    	if user_input == "":
     		user_input = self.window.active_view().file_name()
-        
-        path = sublime.packages_path() + "\\FoxCode\\Bats\\"
+
+    	path = sublime.packages_path() + "\\FoxCode\\Bats\\"
     	self.window.run_command("exec",{"cmd":["start.bat",user_input],"working_dir": path})
-    	pass
