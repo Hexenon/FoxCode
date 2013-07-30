edd098e85248748f07568ffd4b22cd33e8062b9c
Hexenon <enriquebenavidesm@gmail.com>
2013-07-29 18:20:39 -0600

    Create foxpro_brow.py

---
+++ foxpro_brow.py
@@ -0,0 +1,15 @@
+import sublime, sublime_plugin  
+import os
+import re
+class foxpro_brow(sublime_plugin.WindowCommand):  
+    def run(self):
+        self.window.show_input_panel("Browse:", "", self.on_done, None, None)
+        pass
+
+    def on_done(self,user_input):
+      if user_input == "":
+    		user_input = "?"
+
+        path = sublime.packages_path() + "\\Foxpro\\Bats\\"
+    	self.window.run_command("exec",{"cmd":["brow.bat",path + "brow.fxp",user_input],"working_dir": path})
+    	pass
