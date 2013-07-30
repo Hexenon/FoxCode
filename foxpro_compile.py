fc824452d5d66dcf8e321509e3af5d0516c6753d
Hexenon <enriquebenavidesm@gmail.com>
2013-07-29 18:42:27 -0600

    Update foxpro_compile.py

--- foxpro_compile.py
+++ foxpro_compile.py
@@ -6,4 +6,4 @@ class foxpro_compile(sublime_plugin.WindowCommand):
     def run(self):
         fname = self.window.active_view().file_name()
         path = sublime.packages_path() + "\\Foxpro\\Bats\\"
-        self.window.run_command("exec",{"cmd":["compile.bat",path+"compile.fxp",fname],"working_dir": path})
+        self.window.run_command("exec",{"cmd":["start.bat",path+"compile.fxp",fname],"working_dir": path})
