817cd39e8b5d9c4aa592a8bff16e6328e2343947
Hexenon <enriquebenavidesm@gmail.com>
2013-07-30 09:18:49 -0600

    Update foxconsole.py

--- foxconsole.py
+++ foxconsole.py
@@ -3,5 +3,5 @@ import re
 
 class foxconsole(sublime_plugin.WindowCommand):  
     def run(self):
-      path = sublime.packages_path() + "\\Foxpro\\Bats\\"
+      path = sublime.packages_path() + "\\FoxCode\\Bats\\"
     	self.window.run_command("exec",{"cmd":["start.bat"],"working_dir": path})
