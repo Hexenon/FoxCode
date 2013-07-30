6afb967733c16ad4ed796d91189e00e15709ca03
Hexenon <enriquebenavidesm@gmail.com>
2013-07-30 09:31:23 -0600

    Update foxpro_modistru.py

--- foxpro_modistru.py
+++ foxpro_modistru.py
@@ -2,15 +2,14 @@ import sublime, sublime_plugin
 import os
 import re
 
-class foxpro_modistru(sublime_plugin.WindowCommand):  
+class foxpro_run(sublime_plugin.WindowCommand):  
     def run(self):
-        self.window.show_input_panel("Modify Structure:", "", self.on_done, None, None)
+        self.window.show_input_panel("Do ", "", self.on_done, None, None)
     	pass
 
     def on_done(self,user_input):
-      if user_input == "":
-    		user_input ="?"
+    	if user_input == "":
+    		user_input = self.window.active_view().file_name()
 
-        path = sublime.packages_path() + "\\FoxCode\\Bats\\"
-    	self.window.run_command("exec",{"cmd":["start.bat",path+"modistru.fxp",user_input],"working_dir": path})
-    	pass
+    	path = sublime.packages_path() + "\\FoxCode\\Bats\\"
+    	self.window.run_command("exec",{"cmd":["start.bat",user_input],"working_dir": path})
