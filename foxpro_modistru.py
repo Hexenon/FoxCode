e0d980608ac34199c36a560f1f0aa615da85c154
Hexenon <enriquebenavidesm@gmail.com>
2013-07-30 09:19:57 -0600

    Update foxpro_modistru.py

--- foxpro_modistru.py
+++ foxpro_modistru.py
@@ -11,6 +11,6 @@ class foxpro_modistru(sublime_plugin.WindowCommand):
       if user_input == "":
     		user_input ="?"
 
-        path = sublime.packages_path() + "\\Foxpro\\Bats\\"
+        path = sublime.packages_path() + "\\FoxCode\\Bats\\"
     	self.window.run_command("exec",{"cmd":["start.bat",path+"modistru.fxp",user_input],"working_dir": path})
     	pass
