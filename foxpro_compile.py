b362166b9084e5a526c4176568cf21654ab662f7
Hexenon <enriquebenavidesm@gmail.com>
2013-07-31 09:44:55 -0600

    Delete foxpro_compile.py

--- foxpro_compile.py
+++
@@ -1,9 +0,0 @@
-import sublime, sublime_plugin
-import os
-import re
-
-class foxpro_compile(sublime_plugin.WindowCommand):  
-    def run(self):
-        fname = self.window.active_view().file_name()
-    	path = sublime.packages_path() + "\\FoxCode\\Bats\\"
-    	self.window.run_command("exec",{"cmd":["start.bat",path+"compile.fxp",fname],"working_dir": path})
